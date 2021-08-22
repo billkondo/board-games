24c2abfb38d93d4b85304dad65515c1fafa35395
William Hideki Kondo <willkondo@gmail.com>
2021-08-21 21:13:31 -0300

    Removing code from App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('it should show APP text', () => {
+    render(<App />);
+    const textElement = screen.getByText('APP');
+    expect(textElement).toBeVisible();
+  });
 });
